da3e146c78b0777ed2e218bfeb9b27cf636cf513
Jesus Pastor <jnavalon@biit-solutions.com>
2023-10-17 10:45:43 +0200

    missed files from last commit

---
+++ projects/authorization-services-lib/src/lib/constants.ts
@@ -0,0 +1,8 @@
+export class Constants {
+  public static readonly SESSION_STORAGE = class {
+    public static readonly AUTH_TOKEN: string = 'authToken';
+    public static readonly AUTH_EXPIRATION: string = 'authExp';
+    public static readonly USER: string = 'user';
+    public static readonly PERMISSIONS: string = 'permissions';
+  }
+}
