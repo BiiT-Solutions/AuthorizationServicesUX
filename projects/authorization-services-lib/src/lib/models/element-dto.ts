47f484a0a4f0b965421ee3ba202a303cafbdbdbd
Jesus Pastor <jnavalon@biit-solutions.com>
2023-10-27 15:40:32 +0200

    changed id to number/string due to language limit

--- projects/authorization-services-lib/src/lib/models/element-dto.ts
+++ projects/authorization-services-lib/src/lib/models/element-dto.ts
@@ -1,16 +1,16 @@
 import {CreatedElementDto} from "./created-element-dto";
 
-export class ElementDto<ID> extends CreatedElementDto {
-  id: ID;
+export class ElementDto extends CreatedElementDto {
+  id: any;
   updatedAt: Date;
   updatedBy: string;
 
-  public static override clone<ID>(from: ElementDto<ID>): ElementDto<ID> {
-    const to: ElementDto<ID> = new ElementDto();
+  public static override clone(from: ElementDto): ElementDto {
+    const to: ElementDto = new ElementDto();
     ElementDto.copy(from, to);
     return to;
   }
-  public static override copy<ID>(from: ElementDto<ID>, to: ElementDto<ID>): void {
+  public static override copy(from: ElementDto, to: ElementDto): void {
     super.copy(from, to);
     to.id = from.id;
     to.updatedAt = from.updatedAt ? new Date(from.updatedAt) : null;
