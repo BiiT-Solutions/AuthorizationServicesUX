ad033bbc3f542cfc43d0855a7c4a38a712649808
Jesus Pastor <jnavalon@biit-solutions.com>
2023-10-27 15:02:52 +0200

    updated elementDTO

--- projects/authorization-services-lib/src/lib/models/element-dto.ts
+++ projects/authorization-services-lib/src/lib/models/element-dto.ts
@@ -1,16 +1,16 @@
 import {CreatedElementDto} from "./created-element-dto";
 
-export class ElementDto extends CreatedElementDto {
-  id: number;
+export class ElementDto<ID> extends CreatedElementDto {
+  id: ID;
   updatedAt: Date;
   updatedBy: string;
 
-  public static override clone(from: ElementDto): ElementDto {
-    const to: ElementDto = new ElementDto();
+  public static override clone<ID>(from: ElementDto<ID>): ElementDto<ID> {
+    const to: ElementDto<ID> = new ElementDto();
     ElementDto.copy(from, to);
     return to;
   }
-  public static override copy(from: ElementDto, to: ElementDto): void {
+  public static override copy<ID>(from: ElementDto<ID>, to: ElementDto<ID>): void {
     super.copy(from, to);
     to.id = from.id;
     to.updatedAt = from.updatedAt ? new Date(from.updatedAt) : null;
