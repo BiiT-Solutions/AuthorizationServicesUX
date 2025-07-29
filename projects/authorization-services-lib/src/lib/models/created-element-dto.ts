71bfd86654aa9c761997ce5b4fcdc9d60baf3f9f
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-29 15:38:52 +0200

    Adding created on

--- projects/authorization-services-lib/src/lib/models/created-element-dto.ts
+++ projects/authorization-services-lib/src/lib/models/created-element-dto.ts
@@ -1,13 +1,17 @@
 export class CreatedElementDto {
   createdAt: Date;
   createdBy: string;
+  createdOn: string;
 
   public static clone(from: CreatedElementDto): CreatedElementDto {
     const to: CreatedElementDto = new CreatedElementDto();
     CreatedElementDto.copy(from, to);
     return to;
   }
+
   public static copy(from: CreatedElementDto, to: CreatedElementDto): void {
     to.createdAt = from.createdAt ? new Date(from.createdAt) : null;
     to.createdBy = from.createdBy;
-  }}
+    to.createdOn = from.createdOn;
+  }
+}
